6f45527921b1bd10ae10c775b092af1b28138b02
Vijaykumar Singh <singhvjd@gmail.com>
2026-09-03 18:00:02 -0500

    test: prove private CI rejects failures

---
+++ SecureMapReduce/src/test/java/com/cloudera/sa/securewordcount/PrivateRoutingProbeTest.java
@@ -0,0 +1,12 @@
+package com.cloudera.sa.securewordcount;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class PrivateRoutingProbeTest {
+  @Test
+  void intentionalNegativeRoutingProof() {
+    fail("intentional negative routing proof");
+  }
+}
